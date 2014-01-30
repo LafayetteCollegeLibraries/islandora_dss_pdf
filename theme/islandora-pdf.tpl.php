8e8d112610ddb0d92876dbd7b212130582e61a3c
jrgriffiniii <griffinj@lafayette.edu>
2014-01-30 13:44:23 -0500

    DSSSM-357 #time 1d 31m Integrating PDF.js with the Islandora PDF Solution Pack

---
+++ theme/islandora-pdf.tpl.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for the pdf object
+ *
+ * @TODO: Add documentation about this file and the available variables
+ */
+?>
+
+<div class="islandora-pdf-object islandora">
+  <div class="islandora-pdf-content-wrapper clearfix">
+    <?php if (isset($islandora_content)): ?>
+
+      <div class="islandora-pdf-content">
+
+	 <?php print $pdf_viewer; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+  <fieldset class="collapsible collapsed islandora-pdf-metadata">
+  <legend><span class="fieldset-legend"><?php print t('Extended details'); ?></span></legend>
+    <div class="fieldset-wrapper">
+      <dl class="islandora-inline-metadata islandora-pdf-fields">
+        <?php $row_field = 0; ?>
+        <?php foreach ($dc_array as $key => $value): ?>
+          <dt class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print $value['label']; ?>
+          </dt>
+          <dd class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print $value['value']; ?>
+          </dd>
+          <?php $row_field++; ?>
+        <?php endforeach; ?>
+      </dl>
+    </div>
+  </fieldset>
+</div>
